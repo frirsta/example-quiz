636b4f5359e30570c6f04cbcc969ed20144589d5
frirsta <frista-8@hotmail.com>
2022-09-15 09:15:08 +0000

    Add javascript function structures

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
-document.getElementById("name-button").addEventListener("click", function() {
+document.getElementById("name-button").addEventListener("click", function () {
     let inputName = document.getElementById("username").value;
     document.getElementById("name-display").innerHTML = `<h2>Go ${inputName}!</h2>`
 })
+
+// game function
+
+function startQuiz() {
+
+}
+
+function displayQuestion() {
+
+}
+
+function checkAnswer() {}
+
+function correctScore() {}
+
+function incorrectScore() {}
